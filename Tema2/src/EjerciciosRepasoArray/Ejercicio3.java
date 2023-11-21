ae154ed57671ec513755aebd56a55521aeada23e
eduran8291 <eduran8291@ceuandalucia.es>
2023-11-21 16:25:06 +0100

    Ejercicio tres de los ejercicios de repaso de arrays hecho👍

--- Tema2/src/EjerciciosRepasoArray/Ejercicio3.java
+++ Tema2/src/EjerciciosRepasoArray/Ejercicio3.java
@@ -1,10 +1,30 @@
 package EjerciciosRepasoArray;
 
+
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¿Que nota estas buscando?");
+		Integer[] listaNotas = new Integer[] { 7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10 };
+		Integer num = sc.nextInt();
+		Integer posicion = 0;
+		Boolean encontrado = false;
+		for (int i = 0; i < listaNotas.length; i++) {
+			if (listaNotas[i] == num) {
+				encontrado = true;
+				posicion = i;
+				
+				System.out.println("Numero encontrado y esta en la posicion " + posicion);
+			}
+		}
+		if(encontrado == false) {
+			System.out.println("El numero no existe");
+		}else {
+		}
+		sc.close();
 	}
 
 }
