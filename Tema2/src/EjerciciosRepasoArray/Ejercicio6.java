7085811afe1bd8537364047a2829396e002ecac0
eduran8291 <eduran8291@ceuandalucia.es>
2023-11-21 17:25:49 +0100

    Ejercicio 7 de los ejercicios de repaso de arrays

---
+++ Tema2/src/EjerciciosRepasoArray/Ejercicio6.java
@@ -0,0 +1,17 @@
+package EjerciciosRepasoArray;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		Integer[] listaNotas = new Integer[] { 7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10 };
+		
+		for (int i = 0; i < listaNotas.length; i++) {
+			if(i % 2 == 0) {
+				listaNotas[i] = listaNotas[i] * 2;
+			}
+			
+			System.out.print(listaNotas[i] + " ");
+		}
+	}
+
+}
