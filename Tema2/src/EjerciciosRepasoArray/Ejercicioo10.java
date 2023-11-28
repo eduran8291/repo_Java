238d9af9353ff1b8c7321dc40932f2c0099f8324
eduran8291 <eduran8291@ceuandalucia.es>
2023-11-28 16:50:15 +0100

    cambios

---
+++ Tema2/src/EjerciciosRepasoArray/Ejercicioo10.java
@@ -0,0 +1,22 @@
+package EjerciciosRepasoArray;
+
+import java.util.Scanner;
+
+public class Ejercicioo10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¿Cuantos alumnos hay en clase?");
+		Integer respuesta = sc.nextInt();
+		Integer[][] notas = new Integer[respuesta][3];
+		String[] nombre = new String[respuesta];
+		for (int i = 0; i < nombre.length; i++) {
+			do {
+				System.out.println("Dime el nombre del alumno");
+				
+			}while()
+			
+		}
+	}
+
+}
