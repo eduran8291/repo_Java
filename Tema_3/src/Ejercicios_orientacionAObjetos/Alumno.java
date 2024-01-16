d5ccd905d7180cc4f06022586796e8fc2b1d37a1
TECH <TECH@CEP-INFOR6-PC30.andalucia.fusp.ceu>
2024-01-16 15:14:32 +0100

    Ejercicios de orientacion a objetos 28,29

--- Tema_3/src/Ejercicios_orientacionAObjetos/Alumno.java
+++ Tema_3/src/Ejercicios_orientacionAObjetos/Alumno.java
@@ -1,51 +1,63 @@
 package Ejercicios_orientacionAObjetos;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Alumno extends Persona{
+public class Alumno extends Persona {
 
 	private String dni;
 	private Integer nota;
-    private Curso curso;
-	
+	private Curso curso;
+
 	public Curso getCurso() {
 		return curso;
 	}
+
 	public void setCurso(Curso curso) {
 		this.curso = curso;
 	}
+
 	public Alumno(String dni) {
 		this.dni = dni;
 	}
+
 	public void aprobar() {
-		if(nota < 5) {
-		this.nota = 5;
+		if (nota < 5) {
+			this.nota = 5;
 		}
 	}
 
 	public String getDni() {
 		return dni;
 	}
+
 	public void setDni(String dni) {
-		this.dni = dni.toUpperCase();
+		if (dni != null) {
+			this.dni = dni.toUpperCase();
+		}
+		this.dni = dni;
 	}
-	
+
 	public Integer getNota() {
 		return nota;
 	}
+
 	@Override
 	public String toString() {
 		return "Alumno [dni=" + dni + ", nota=" + nota + ", curso=" + curso + "]";
 	}
+
 	public void setNota(Integer nota) {
 		this.nota = nota;
-		
-		
+
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(dni);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -57,8 +69,17 @@ public class Alumno extends Persona{
 		Alumno other = (Alumno) obj;
 		return Objects.equals(dni, other.dni);
 	}
-	
-	
-	
+
+	public Boolean validarDni() {
+		if (this.dni != null && !this.dni.isEmpty()) {
+			Pattern patron = Pattern.compile("[0-9]{7,8}[A-Z a-z]");
+			Matcher match = patron.matcher(this.dni);
+			if (match.matches()) {
+				return true;
+			} 
+			return false;
+		}
+		return false;
+	}
 
 }
--- Tema_3/src/Ejercicios_orientacionAObjetos/ejercicioAlumno.java
+++ Tema_3/src/Ejercicios_orientacionAObjetos/ejercicioAlumno.java
@@ -6,45 +6,20 @@ public class ejercicioAlumno {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		  System.out.println("Introduce los siguientes datos-->");
-		  
-		  System.out.print("Dni:"); String dni = sc.nextLine();
-		  
-		  Alumno alumno1 = new Alumno(dni);
-		  
-		  
-		  System.out.print("Edad:"); alumno1.setEdad(sc.nextInt());
-		  
-		  sc.nextLine();
-		  
-		  System.out.print("Nombre:"); alumno1.setNombre(sc.nextLine());
-		 
-		  System.out.print("Nota:"); alumno1.setNota(sc.nextInt());
-		  
-		  Curso curso = new Curso();
-		  
-		  alumno1.setCurso(curso);
-		  
-		  
-		  System.out.print("Estos son los siguientes datos" + alumno1);
-		 
-		  
-		  alumno1.aprobar();
-		 
-		  System.out.println(alumno1.getNota());
-		 
+
+		Curso curso = new Curso();
 
 		Alumno[] alumnos = new Alumno[3];
-		
+
 		// Curso para todos
 		curso.setIdentificador("1");
 		curso.setDescripcion("DAM-DAW");
 
 		for (int i = 0; i < alumnos.length; i++) {
-			
+			do {
 			System.out.println("Introduce el DNI");
 			alumnos[i] = new Alumno(sc.nextLine());
+			}while(!alumnos[i].validarDni());
 			
 			System.out.println("Introduce el nombre");
 			alumnos[i].setNombre(sc.nextLine());
@@ -56,10 +31,12 @@ public class ejercicioAlumno {
 			System.out.println("Introduce la nota");
 			alumnos[i].setNota(sc.nextInt());
 			sc.nextLine();
+			System.out.println(alumnos[i].validarDni());
+
 		}
-		if(alumnos[0].equals(alumnos[1]) || alumnos[0].equals(alumnos[2]) || alumnos[1].equals(alumnos[2])){
+		if (alumnos[0].equals(alumnos[1]) || alumnos[0].equals(alumnos[2]) || alumnos[1].equals(alumnos[2])) {
 			System.out.println("ERROR");
-		}else {
+		} else {
 			System.out.println("No hay alumnos con el mismo DNI");
 		}
 	}
